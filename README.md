765c7830dba611d96827bc2bafd81645434440eb
Hamidreza Firouzi <hamdireza4dev@gmail.com>
2022-04-14 19:06:32 +0430

    add animation delay future

---
+++ README.md
@@ -0,0 +1,69 @@
+# tailwindcss animatecss plugin
+This plugin helps you use [animateCss](https://animate.style/) animation in tailwind.
+
+### Initial setup
+First of all add the plugin to the "tailwind.config.js" file :
+```javascript
+// tailwind.config.js
+module.exports = {
+  content: ['...'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('tailwind-animatecss')],
+};
+```
+
+#### examples :
+```html
+<div class="animate-animated animate-zoomInDown"></div>
+```
+
+## animation delay
+animation delays are inspired by [tailwind-animation-delay](https://github.com/hamidreza4dev/tailwindcss-animation-delay-plugin)
+
+#### examples :
+```html
+<div class="animate-ping animation-delay-1"></div>
+<div class="animate-ping animation-delay-2"></div>
+<div class="animate-ping animation-delay-3"></div>
+<div class="animate-ping animation-delay-4"></div>
+<div class="animate-ping animation-delay-5"></div>
+<div class="animate-ping animation-delay-6"></div>
+<div class="animate-ping animation-delay-7"></div>
+<div class="animate-ping animation-delay-8"></div>
+<div class="animate-ping animation-delay-9"></div>
+<div class="animate-ping animation-delay-10"></div>
+```
+### customization
+These are the allowed values by default :
+```json
+{
+  1: "250",
+  2: "500",
+  3: "750",
+  4: "1000",
+  5: "1250",
+  6: "1500",
+  7: "1750",
+  8: "2000",
+  9: "2250",
+  10: "2500"
+}
+```
+But you can still use your own values by changing the "animationDelay" property :
+```javascript
+// tailwind.config.js
+module.exports = {
+  content: ['...'],
+  theme: {
+    extend: {},
+    animationDelay: {
+      1: '1000',
+      2: '2000',
+      3: '3000',
+    },
+  },
+  plugins: [require('tailwind-animatecss')],
+};
+```
--- tailwind-animatecss.js
+++ tailwind-animatecss.js
@@ -1,21 +1,58 @@
 const plugin = require('tailwindcss/plugin');
-const animationsConfig = require('./config');
+const { animationsConfig } = require('./config');
 
-const { fallbackFix, defaultAnimation } = require('./fallback');
+const { fallbackFix } = require('./fallback');
 
 const tailwindAnimateCss = plugin.withOptions(
   function (options) {
     return function ({ addUtilities, theme, e }) {
-      const values = theme('animations');
-      var utilities = Object.entries(values).map(([key, value]) => {
-        return {
-          [`.${e(`${key}`)}`]: {
-            ...value,
-          },
-        };
-      });
-      addUtilities(fallbackFix);
-      addUtilities(utilities);
+      const animationDelayValues = theme('animationDelay');
+      var animationDelayUtilities = Object.entries(animationDelayValues).map(
+        ([key, value]) => {
+          return {
+            [`.${e(`animation-delay-${key}`)}`]: {
+              'animation-delay': `${value}ms`,
+            },
+          };
+        }
+      );
+      addUtilities(animationDelayUtilities);
+
+      const animationsValues = theme('animations');
+      var animationsUtilities = Object.entries(animationsValues).map(
+        ([key, value]) => {
+          return {
+            [`.${e(`${key}`)}`]: {
+              ...value,
+            },
+          };
+        }
+      );
+
+      // prettier-ignore
+      (function () {
+        const animatedSpeed = options?.animatedSpeed ? options?.animatedSpeed : 1000;
+        const heartBeatSpeed = options?.heartBeatSpeed ? options?.heartBeatSpeed : 1000;
+        const hingeSpeed = options?.hingeSpeed ? options?.hingeSpeed : 2000;
+        const bounceInSpeed = options?.bounceInSpeed ? options?.bounceInSpeed : 750;
+        const bounceOutSpeed = options?.bounceOutSpeed ? options?.bounceOutSpeed : 750;
+        const animationDelaySpeed = options?.animationDelaySpeed ? options?.animationDelaySpeed : 500;
+        const opacity = options?.opacity ? options?.opacity : 1;
+        const animationsLength = animationsConfig({
+          animatedSpeed,
+          heartBeatSpeed,
+          hingeSpeed,
+          bounceInSpeed,
+          bounceOutSpeed,
+          animationDelaySpeed,
+          opacity,
+        });
+
+        if (Object.entries(animationsLength.animation).length === Object.entries(theme('animation')).length) {
+          addUtilities(fallbackFix);
+          addUtilities(animationsUtilities);
+        }
+      })();
     };
   },
   // prettier-ignore
@@ -40,6 +77,19 @@ const tailwindAnimateCss = plugin.withOptions(
             animationDelaySpeed,
             opacity,
           }),
+        },
+
+        animationDelay: {
+          1: '250',
+          2: '500',
+          3: '750',
+          4: '1000',
+          5: '1250',
+          6: '1500',
+          7: '1750',
+          8: '2000',
+          9: '2250',
+          10: '2500',
         },
       },
     };
